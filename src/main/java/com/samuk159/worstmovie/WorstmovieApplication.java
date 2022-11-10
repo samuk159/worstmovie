2509ef04cd1fac28cc044f29b04d7a0d6309318a
Samuel Alles Remlinger <samu_65@hotmail.com>
2022-11-10 20:39:30 -0300

    primeiro commit

---
+++ src/main/java/com/samuk159/worstmovie/WorstmovieApplication.java
@@ -0,0 +1,13 @@
+package com.samuk159.worstmovie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorstmovieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorstmovieApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/samuk159/worstmovie/WorstmovieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.samuk159.worstmovie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorstmovieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
