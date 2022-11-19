59d85aca5360c8e818d6082da580b74794eaa791
Samuel Alles Remlinger <samu_65@hotmail.com>
2022-11-19 06:26:52 -0300

    update e delete funcionando

--- src/main/java/com/samuk159/worstmovie/controller/MovieController.java
+++ src/main/java/com/samuk159/worstmovie/controller/MovieController.java
@@ -9,9 +9,11 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -52,6 +54,24 @@ public class MovieController {
 		
 		res.setData(repository.save(movie));
 		return ResponseEntity.status(HttpStatus.CREATED).body(res);
+	}
+	
+	@PutMapping("/{id}")
+	public ResponseEntity<Movie> update(@PathVariable Long id, @Valid @RequestBody Movie movie) {
+		Optional<Movie> opt = repository.findById(id);
+		
+		if (opt.isPresent()) {
+			movie.setId(id);
+			return ResponseEntity.ok(repository.save(movie));
+		} else {
+			return ResponseEntity.notFound().build();
+		}
+	}
+	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Movie> delete(@PathVariable Long id) {
+		repository.deleteById(id);
+		return ResponseEntity.noContent().build();
 	}
 	
 }
